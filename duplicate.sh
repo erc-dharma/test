3df05cb5aa5bccab3871960d86fd4d5a3fadc7b0
ajaniak <33328906+ajaniak@users.noreply.github.com>
2021-03-10 12:14:57 +0100

    changing the context for the ocndition

--- duplicate.sh
+++ duplicate.sh
@@ -3,6 +3,6 @@ dirname=./texts/xml
 find $dirname -type f | sed 's_.*/__' | sort|  uniq -d|
 while read fileName
 do
-if find $dirname -type f | grep "$fileName"; then echo ::set-output name=duplicate_filename::$(find $dirname -type f | grep "$fileName");
+if [ -n $(find $dirname -type f | grep "$fileName")]; then echo ::set-output name=duplicate_filename::$(find $dirname -type f | grep "$fileName");
 fi;
 done
