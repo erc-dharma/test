dce190f327262e83bc4b9a340ae78868e0e4c891
ajaniak <33328906+ajaniak@users.noreply.github.com>
2021-03-10 09:51:19 +0100

    adding a failure condition

--- duplicate.sh
+++ duplicate.sh
@@ -5,4 +5,9 @@ find $dirname -type f | sed 's_.*/__' | sort|  uniq -d|
 while read fileName
 do
 find $dirname -type f | grep "$fileName"
-done)
+done
+
+if find $dirname -type f | grep "$fileName" != 0 ; then
+  echo "Game over!"
+  exit 1
+fi)
