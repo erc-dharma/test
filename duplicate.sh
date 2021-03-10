506dfa75a53b8dafa140f75b8a6c0fb802142886
ajaniak <33328906+ajaniak@users.noreply.github.com>
2021-03-10 10:02:37 +0100

    trying to clean the output result

--- duplicate.sh
+++ duplicate.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 dirname=./texts/xml
-echo ::set-output name=duplicate_filename::$(
+
 find $dirname -type f | sed 's_.*/__' | sort|  uniq -d|
 while read fileName
 do
-find $dirname -type f | grep "$fileName"
+echo ::set-output name=duplicate_filename::$( find $dirname -type f | grep "$fileName")
 done
-
-if find $dirname -type f | grep "$fileName" != 0 ; then
-  echo "Game over!"
-  exit 1
-fi)
