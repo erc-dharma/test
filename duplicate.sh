52d33493b5b8c1704663b3002b6e85bb363395e4
ajaniak <33328906+ajaniak@users.noreply.github.com>
2021-09-08 10:28:20 +0200

    new reset to test and config search bar for ARIE

--- duplicate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-dirname=./texts/xml
-find $dirname -type f | sed 's_.*/__' | sort|  uniq -d|
-while read fileName
-do
-if [ -n $(find $dirname -type f | grep "$fileName")]; then echo ::set-output name=duplicate_filename::$(find $dirname -type f | grep "$fileName");
-fi;
-done
