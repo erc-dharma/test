984426060c103b51e48d2c85f1c841db1e1729a9
ajaniak <33328906+ajaniak@users.noreply.github.com>
2021-09-09 12:44:17 +0200

    adding search into search page

---
+++ build/build.js
@@ -0,0 +1,13 @@
+React = require('react')
+ReactDOM = require('react-dom')
+ReactRouter = require('react-router')
+ReactGA = require('react-ga')
+ReactDisqusThread = require('react-disqus-thread')
+
+Marked = require('marked')
+
+SODB = require('sodb')
+
+OctoNode = require('octonode')
+
+Github = OctoNode.client()
