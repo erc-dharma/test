7cc82e8cb5fc0b0bb0c82c94494ab68fb3e5abbb
ajaniak <33328906+ajaniak@users.noreply.github.com>
2021-09-09 16:21:27 +0200

    new push

--- js/search.js
+++ js/search.js
@@ -33,27 +33,30 @@
   var searchTerm = getQueryVariable('query');
 
   if (searchTerm) {
-    document.getElementById('search-box').setAttribute("value", searchTerm);
+    document.getElementById('search-box').setAttribute('value', searchTerm);
 
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
       this.field('id');
       this.field('title', { boost: 10 });
-      this.field('description');
-      this.field('url');
+      this.field('author');
+      this.field('category');
+      this.field('content');
 
-    for (var key in window.store) { // Add the data to lunr
-      this.add({
-        'id': key,
-        'title': window.store[key].title,
-        'description': window.store[key].description,
-        'url': window.store[key].url
-      });
-}
-});
-      var results = idx.search(searchTerm); // Get lunr to perform a search
-      displaySearchResults(results, window.store); // We'll write this in the next section
-    }
+      for (var key in window.store) { // Add the data to lunr
+        this.add({
+          'id': key,
+          'title': window.store[key].title,
+          'author': window.store[key].author,
+          'category': window.store[key].category,
+          'content': window.store[key].content
+        });
+      }
+
+    });
+
+    var results = idx.search(searchTerm); // Get lunr to perform a search
+    displaySearchResults(results, window.store); // We'll write this in the next section
+
   }
-})();
